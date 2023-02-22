edde9325b3774e3a806dbbb638d01426470650c8
Samantha131821 <samanthaleecamerlengo@gmail.com>
2023-02-22 13:50:23 -0500

    future forecast info

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 let apiKey="fc26d0c630f53a8cc7245122c5d0f9e5";
 
+let today = dayjs();
+let currentDay = $('#currentDate');
+currentDay.text(today.format('dddd'));
+
 
 $("#search").on("click", ()=> {
     let searchInput = $("#search-input");
@@ -23,28 +27,23 @@ $("#search").on("click", ()=> {
 
         })
         .then((data) => {
-            console.log('currentData: ', data);
+            // console.log('currentData: ', data);
 
             let cityName = data.name;
-            //console.log(cityName)
             $(".city-name").append(cityName);
 
             let currentTemp = data.main.temp;
-            // console.log(currentTemp);
-            $("#currentTemp").append("Temperature: ", currentTemp);
+            $("#currentTemp").append("<b>Temperature: </b>", currentTemp);
 
             let weatherConditions = data.weather[0].main;
-            // console.log("Test: ", weatherConditions);
-            $("#currentCond").append("Conditions: ", weatherConditions);
+            $("#currentCond").append("<b>Conditions: </b>", weatherConditions);
 
 
            let windSpeed = data.wind.speed;
-            //console.log ('wind speed ', windSpeed)
-            $("#currentWindSpeed").append("Wind Speed: ", windSpeed);
+            $("#currentWindSpeed").append("<b>Wind Speed: </b>", windSpeed);
 
            let humidity = data.main.humidity;
-            //console.log ('humidity', humidity)
-            $("#currentHumidity").append("Humidity: ", humidity);
+            $("#currentHumidity").append("<b>Humidity: </b>", humidity);
 
         });
 
@@ -61,7 +60,45 @@ $("#search").on("click", ()=> {
             const dayThreeWeather = data.list[16]
             const dayFourWeather = data.list[24]
             const dayFiveWeather = data.list[32]
-            // console.log(dayFiveWeather)
+            
+            // Day One
+            let dayOneTemp = dayOneWeather.main.temp;
+            $("#futureTemp1").append("<b>Temperature: </b>", dayOneTemp);
+
+            let weatherCond1 = dayOneWeather.weather[0].main;
+            $("#futureCond1").append("<b>Conditions: </b>", weatherCond1);
+
+            // Day Two
+            let dayTwoTemp = dayTwoWeather.main.temp;
+            $("#futureTemp2").append("<b>Temperature: <br></b>", dayTwoTemp);
+
+            let weatherCond2 = dayTwoWeather.weather[0].main;
+            $("#futureCond2").append("<b>Conditions: </b>", weatherCond2);
+
+
+            // Day Three
+            let dayThreeTemp = dayThreeWeather.main.temp;
+            $("#futureTemp3").append("<b>Temperature: </b>", dayThreeTemp);
+           
+            let weatherCond3 = dayThreeWeather.weather[0].main;
+            $("#futureCond3").append("<b>Conditions: </b>", weatherCond3);
+
+
+            // Day Four
+            let dayFourTemp = dayFourWeather.main.temp;
+            $("#futureTemp4").append("<b>Temperature: </b>", dayFourTemp);
+
+            let weatherCond4 = dayFourWeather.weather[0].main;
+            $("#futureCond4").append("<b>Conditions: </b>", weatherCond4);
+
+            // Day Five
+            let dayFiveTemp = dayFiveWeather.main.temp;
+            $("#futureTemp5").append("<b>Temperature: </b>", dayFiveTemp);
+
+            let weatherCond5 = dayFiveWeather.weather[0].main;
+            $("#futureCond5").append("<b>Conditions: </b>", weatherCond5);
+
+
         })  
     })
 
