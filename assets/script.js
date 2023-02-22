50acf19b1670960182221c1624bba6dcb07e7727
Samantha131821 <samanthaleecamerlengo@gmail.com>
2023-02-21 22:50:45 -0500

    current weather displayed

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 let apiKey="fc26d0c630f53a8cc7245122c5d0f9e5";
 
-let forecastWeather = []
-
 
 $("#search").on("click", ()=> {
     let searchInput = $("#search-input");
@@ -16,44 +14,55 @@ $("#search").on("click", ()=> {
         console.log('response:', res)
         let lat = res[0].lat;
         let lon = res[0].lon;
-        let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        var currentUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+        let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
+        $.ajax({
+            url:currentUrl,
+            method:'GET'
+
+        })
+        .then((data) => {
+            console.log('currentData: ', data);
+
+            let cityName = data.name;
+            //console.log(cityName)
+            $(".city-name").append(cityName);
+
+            let currentTemp = data.main.temp;
+            // console.log(currentTemp);
+            $("#currentTemp").append("Temperature: ", currentTemp);
+
+            let weatherConditions = data.weather[0].main;
+            // console.log("Test: ", weatherConditions);
+            $("#currentCond").append("Conditions: ", weatherConditions);
+
+
+           let windSpeed = data.wind.speed;
+            //console.log ('wind speed ', windSpeed)
+            $("#currentWindSpeed").append("Wind Speed: ", windSpeed);
+
+           let humidity = data.main.humidity;
+            //console.log ('humidity', humidity)
+            $("#currentHumidity").append("Humidity: ", humidity);
+
+        });
+
+    
         $.ajax({
             url:forecastUrl,
             method:'GET'
 
         })
-        .then((data) => {
-            console.log('data: ', data);
-
-            let cityName = data.city.name;
-            //console.log(cityName)
-
-            let weatherConditions = data.list[0].weather[0].main;
-            //console.log('Conditions', weatherConditions)
-
-           let windSpeed = data.list[0].wind.speed;
-            //console.log ('wind speed ', windSpeed)
-
-           let humidity = data.list[0].main.humidity;
-            //console.log ('humidity', humidity)
-            
-           forecastWeather.push(data.list)
-
-        
-            for (let i = 0; i < forecastWeather.length; i++) {
-                // console.log(forecastWeather[i][0].main.temp)
-                let tempFarenheit =  1.8*(forecastWeather[i][0].main.temp-273) + 32; 
-                // console.log('temp in farenheit: ', Math.floor(tempFarenheit))
-
-            }
-        
-        })
-
-        // $(document).ready(function(){
-        //     $('city-name').append() 
-        // })
-
+        .then((data) =>{
+            console.log("forecastData: ", data)
+            const dayOneWeather = data.list[0]
+            const dayTwoWeather = data.list[8]
+            const dayThreeWeather = data.list[16]
+            const dayFourWeather = data.list[24]
+            const dayFiveWeather = data.list[32]
+            // console.log(dayFiveWeather)
+        })  
     })
 
 
