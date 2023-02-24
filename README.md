8a372899858debaf42524e6bffdbdfedf5abac64
Samantha131821 <samanthaleecamerlengo@gmail.com>
2023-02-24 14:21:25 -0500

    updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # weathersite
+
+The purpose of this project was to create a weather app to display current weather as well as a 5 day forecast. 
+
+The user can search a city and the results will be displayed as well as saved in Local Storage under Previous Searches. 
+
+![Image](/assets/Capture.PNG)
+
+
+
+https://github.com/Samantha131821/weathersite
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,23 @@
 let apiKey="fc26d0c630f53a8cc7245122c5d0f9e5";
+let storedWeather = JSON.parse(localStorage.getItem("weather")) || [];
 
-let today = dayjs();
-let currentDay = $('#currentDate');
-currentDay.text(today.format('dddd'));
+// if(storedWeather.length != 0){
+//     updateForecastSearch(0)
+// }
 
+
+
+// function updateForecastSearch(index){
+//     $(".container").empty();
+// }
 
 
 $("#search").on("click", ()=> {
     let searchInput = $("#search-input");
     let userCity = searchInput.val();
     var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity},&limit=1&appid=${apiKey}`;
-    // localStorage.setItem('searchHistory ', userCity);
-    // localStorage.getItem('searchHistory');
-    
+      
+
 
     // Current Forecast
 
@@ -20,6 +25,7 @@ $("#search").on("click", ()=> {
         url:geoUrl,
         method:'GET'
     })
+
     .then((res) => {
         console.log('response:', res)
         let lat = res[0].lat;
@@ -33,36 +39,60 @@ $("#search").on("click", ()=> {
 
         })
         .then((data) => {
-        console.log('currentData: ', data);
+            console.log('currentData: ', data);
             
+            //$("#info").empty();
+
+            let today = dayjs();
+            let currentDay = $('#currentDate');
+            currentDay.text(today.format('M/D/YYYY'));
 
             let cityName = data.name;
             $(".city-name").append(cityName);
 
             let currentTemp = data.main.temp;
-            $("#currentTemp").append("<b>Temperature: </b>", Math.floor(currentTemp));
+            $("#currentTemp").append(Math.floor(currentTemp) + "°");
 
             let weatherConditions = data.weather[0].main;
             $("#currentCond").append("<b>Conditions: </b>", weatherConditions);
         
 
-            // Weather Conditions icon
+            // Weather Conditions icon- current weather
             let imgIcon = data.weather[0].icon; 
-            let imgUrl = "http://openweathermap.org/img/w/" + imgIcon +".png";
-            $("current-weather-icon").attr('src', imgUrl);
-            console.log("Icon: ", imgIcon)
+            let imgUrl = "http://openweathermap.org/img/wn/" + imgIcon + ".png";
+            $("#current-weather-icon").attr('src', imgUrl);
+            
     
 
            let windSpeed = data.wind.speed;
-            $("#currentWindSpeed").append("<b>Wind Speed: </b>", windSpeed + " mph");
+            $("#currentWindSpeed").append("<b>Wind: </b>", windSpeed + " mph");
 
            let humidity = data.main.humidity;
             $("#currentHumidity").append("<b>Humidity: </b>", humidity);
 
-    
+            updateStoredWeather(cityName, currentTemp, weatherConditions, windSpeed, humidity)
         });
 
+        function updateStoredWeather(cityName, currentTemp, weatherConditions, windSpeed, humidity){
+            let weatherObj = {
+                cityName:'',
+                currentTemp:'',
+                weatherConditions:'',
+                windSpeed:'',
+                humidity:''
+            }
+
+            weatherObj.cityName = cityName;
+            weatherObj.currentTemp = currentTemp;
+            weatherObj.weatherConditions = weatherConditions;
+            weatherObj.windSpeed = windSpeed;
+            weatherObj.humidity = humidity;
+            storedWeather.push(weatherObj);
+            localStorage.setItem("weather", JSON.stringify(storedWeather))
+        }
+
     
+
 
 
         // 5 Day Forecast
@@ -73,7 +103,9 @@ $("#search").on("click", ()=> {
 
         })
         .then((data) =>{
-            // console.log("forecastData: ", data)
+
+            
+            console.log("forecastData: ", data)
             const dayOneWeather = data.list[0]
             const dayTwoWeather = data.list[8]
             const dayThreeWeather = data.list[16]
@@ -82,24 +114,15 @@ $("#search").on("click", ()=> {
             
 
 
-            // let futureDays = []
-
-            // for (let i = 0; i < currentDay; i++) {
-            //     console.log(futureDays)
-            //     $('.futureDate').append(futureDays)
-
-            // }
-
-
             // Day One
             let dayOneTemp = dayOneWeather.main.temp;
-            $("#futureTemp1").append("<b>Temperature: </b>", Math.floor(dayOneTemp));
+            $("#futureTemp1").append(Math.floor(dayOneTemp) + "°");
 
             let weatherCond1 = dayOneWeather.weather[0].main;
             $("#futureCond1").append("<b>Conditions: </b>", weatherCond1);
 
             let windSpeed1 = dayOneWeather.wind.speed;
-            $("#futureWindSpeed1").append("<b>Wind Speed: </b>", windSpeed1 + " mph");
+            $("#futureWindSpeed1").append("<b>Wind: </b>", windSpeed1 + " mph");
 
             let humidity1 = dayOneWeather.main.humidity;
             $("#futureHumidity1").append("<b>Humidity: </b>", humidity1);
@@ -108,17 +131,20 @@ $("#search").on("click", ()=> {
             let imgUrl1 = "http://openweathermap.org/img/w/" + imgIcon1 +".png";
             $("#weather-icon1").attr('src', imgUrl1);
            
+            // console.log($("#dayOne > .futureDate"))
+            let dateTxt1 = new Date(dayOneWeather.dt_txt).toLocaleDateString();
+            $("#dayOne > .futureDate").text(dateTxt1);
 
             // Day Two
             let dayTwoTemp = dayTwoWeather.main.temp;
-            $("#futureTemp2").append("<b>Temperature: </b>", Math.floor(dayTwoTemp));
+            $("#futureTemp2").append(Math.floor(dayTwoTemp) + "°");
 
             let weatherCond2 = dayTwoWeather.weather[0].main;
             $("#futureCond2").append("<b>Conditions: </b>", weatherCond2);
 
             
             let windSpeed2 = dayTwoWeather.wind.speed;
-            $("#futureWindSpeed2").append("<b>Wind Speed: </b>", windSpeed2 + " mph");
+            $("#futureWindSpeed2").append("<b>Wind: </b>", windSpeed2 + " mph");
 
             let humidity2 = dayTwoWeather.main.humidity;
             $("#futureHumidity2").append("<b>Humidity: </b>", humidity2);
@@ -127,17 +153,19 @@ $("#search").on("click", ()=> {
             let imgUrl2 = "http://openweathermap.org/img/w/" + imgIcon2 +".png";
             $("#weather-icon2").attr('src', imgUrl2);
     
+            let dateTxt2 = new Date(dayTwoWeather.dt_txt).toLocaleDateString();
+            $("#dayTwo > .futureDate").text(dateTxt2);
 
             // Day Three
             let dayThreeTemp = dayThreeWeather.main.temp;
-            $("#futureTemp3").append("<b>Temperature: </b>", Math.floor(dayThreeTemp));
+            $("#futureTemp3").append(Math.floor(dayThreeTemp) + "°");
            
             let weatherCond3 = dayThreeWeather.weather[0].main;
             $("#futureCond3").append("<b>Conditions: </b>", weatherCond3);
 
             
             let windSpeed3 = dayThreeWeather.wind.speed;
-            $("#futureWindSpeed3").append("<b>Wind Speed: </b>", windSpeed3 + " mph");
+            $("#futureWindSpeed3").append("<b>Wind: </b>", windSpeed3 + " mph");
 
             let humidity3 = dayThreeWeather.main.humidity;
             $("#futureHumidity3").append("<b>Humidity: </b>", humidity3);
@@ -145,16 +173,19 @@ $("#search").on("click", ()=> {
             let imgIcon3 = dayThreeWeather.weather[0].icon; 
             let imgUrl3 = "http://openweathermap.org/img/w/" + imgIcon3 +".png";
             $("#weather-icon3").attr('src', imgUrl3);
+
+            let dateTxt3 = new Date(dayThreeWeather.dt_txt).toLocaleDateString();
+            $("#dayThree> .futureDate").text(dateTxt3);
            
             // Day Four
             let dayFourTemp = dayFourWeather.main.temp;
-            $("#futureTemp4").append("<b>Temperature: </b>", Math.floor(dayFourTemp));
+            $("#futureTemp4").append(Math.floor(dayFourTemp) + "°");
 
             let weatherCond4 = dayFourWeather.weather[0].main;
             $("#futureCond4").append("<b>Conditions: </b>", weatherCond4);
             
             let windSpeed4 = dayFourWeather.wind.speed;
-            $("#futureWindSpeed4").append("<b>Wind Speed: </b>", windSpeed4 + " mph");
+            $("#futureWindSpeed4").append("<b>Wind: </b>", windSpeed4 + " mph");
 
             let humidity4 = dayFourWeather.main.humidity;
             $("#futureHumidity4").append("<b>Humidity: </b>", humidity4);
@@ -162,17 +193,20 @@ $("#search").on("click", ()=> {
             let imgIcon4 = dayFourWeather.weather[0].icon; 
             let imgUrl4 = "http://openweathermap.org/img/w/" + imgIcon4 +".png";
             $("#weather-icon4").attr('src', imgUrl4);
+
+            let dateTxt4 = new Date(dayFourWeather.dt_txt).toLocaleDateString();
+            $("#dayFour> .futureDate").text(dateTxt4);
           
 
             // Day Five
             let dayFiveTemp = dayFiveWeather.main.temp;
-            $("#futureTemp5").append("<b>Temperature: </b>", Math.floor(dayFiveTemp));
+            $("#futureTemp5").append(Math.floor(dayFiveTemp) + "°");
 
             let weatherCond5 = dayFiveWeather.weather[0].main;
             $("#futureCond5").append("<b>Conditions: </b>", weatherCond5);
 
             let windSpeed5 = dayFiveWeather.wind.speed;
-            $("#futureWindSpeed5").append("<b>Wind Speed: </b>", windSpeed5) + " mph";
+            $("#futureWindSpeed5").append("<b>Wind: </b>", windSpeed5) + " mph";
 
             let humidity5 = dayFiveWeather.main.humidity;
             $("#futureHumidity5").append("<b>Humidity: </b>", humidity5);
@@ -180,16 +214,14 @@ $("#search").on("click", ()=> {
             let imgIcon5 = dayFiveWeather.weather[0].icon; 
             let imgUrl5 = "http://openweathermap.org/img/w/" + imgIcon5 +".png";
             $("#weather-icon5").attr('src', imgUrl5);
-           
 
-            
+            let dateTxt5 = new Date(dayFiveWeather.dt_txt).toLocaleDateString();
+            $("#dayFive> .futureDate").text(dateTxt5);
+          
         })  
+    
     })
 
-   
-
-    //clear so if user searchs again it refreshes
-
-
+    
 
 })
